3e6827dde009166810cb1c319f46abd3be829a8a
jader_tao <jader_tao@qq.com>
2019-01-22 23:42:37 +0800

    dev: map over image and read pixel info

--- main.go
+++ main.go
@@ -6,32 +6,91 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+	"io"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	imagePath := "./images/1.jpg"
-	file, _ := os.Open(imagePath)
+	count := make(map[Pixel]uint32)
+	// order := make(map[uint32]Pixel)
 
-	defer file.Close()
-	img, _, err := image.Decode(file)
-	if err != nil {
-		fmt.Println("err = ", err)
-		return
-	}
-
-	b := img.Bounds()
-	width := b.Max.X
-	height := b.Max.Y
-
-	fmt.Println("width = ", width)
-	fmt.Println("height = ", height)
-
-	for i := 0; i < width; i++ {
-		for j := 0; j < height; j++ {
-			r, g, b, a := img.At(i, j).RGBA()
-			println(i, j, r, g, b, a)
+	var pixelCountReducer = func(p Pixel) {
+		if val, ok := count[p]; ok {
+			count[p] = val + 1
+		} else {
+			count[p] = 1
 		}
 	}
 
+	rd, err := ioutil.ReadDir("./images")
+	if err != nil {
+		fmt.Println("read dir fail:", err)
+	}
+
+	for _, pic := range rd {
+		name := "./images/" + pic.Name()
+		fmt.Println(name)
+		file, _ := os.Open(name)
+
+		iterateImage(file, pixelCountReducer)
+	}
+
+	fmt.Println(count)
+
+}
+
+func iterateImage(file io.Reader, callback func(p Pixel)) error {
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return err
+	}
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	for y := bounds.Min.Y; y < height; y++ {
+		for x := bounds.Min.X; x < width; x++ {
+			callback(rgbaToPixel(img.At(x, y).RGBA()))
+		}
+	}
+
+	return nil
+}
+
+// Get the bi-dimensional pixel array
+func getPixels(file io.Reader) ([][]Pixel, error) {
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	var pixels [][]Pixel
+	for y := bounds.Min.Y; y < height; y++ {
+		var row []Pixel
+		for x := bounds.Min.X; x < width; x++ {
+			row = append(row, rgbaToPixel(img.At(x, y).RGBA()))
+		}
+		pixels = append(pixels, row)
+	}
+
+	return pixels, nil
+}
+
+// img.At(x, y).RGBA() returns four uint32 values; we want a Pixel
+func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
+	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+}
+
+// Pixel struct example
+type Pixel struct {
+	R int
+	G int
+	B int
+	A int
 }
